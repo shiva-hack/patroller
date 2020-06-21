cde8c6ff01e32f21c72ea7cc2dac4603800bc10f
Shivam Dhruva <shivam@everledger.io>
2020-06-21 15:30:30 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+patroller
+=========
+
+
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/patroller.svg)](https://npmjs.org/package/patroller)
+[![Downloads/week](https://img.shields.io/npm/dw/patroller.svg)](https://npmjs.org/package/patroller)
+[![License](https://img.shields.io/npm/l/patroller.svg)](https://github.com/shiva-hack/patroller/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g patroller
+$ patroller COMMAND
+running command...
+$ patroller (-v|--version|version)
+patroller/0.0.0 darwin-x64 node-v12.16.3
+$ patroller --help [COMMAND]
+USAGE
+  $ patroller COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import {Command, flags} from '@oclif/command'
+
+class Patroller extends Command {
+  static description = 'describe the command here'
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({char: 'v'}),
+    help: flags.help({char: 'h'}),
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({char: 'n', description: 'name to print'}),
+    // flag with no value (-f, --force)
+    force: flags.boolean({char: 'f'}),
+  }
+
+  static args = [{name: 'file'}]
+
+  async run() {
+    const {args, flags} = this.parse(Patroller)
+
+    const name = flags.name ?? 'world'
+    this.log(`hello ${name} from ./src/index.ts`)
+    if (args.file && flags.force) {
+      this.log(`you input --force and --file: ${args.file}`)
+    }
+
+    // 1) check the dependencies for missing / unused using depcheck
+    // 2) clean the package.json using clean-package-json
+    // 3) check the licenses using license-checker
+  }
+}
+
+export = Patroller
